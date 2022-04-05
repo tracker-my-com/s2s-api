58edbd6d26de03aa0b78b25b8c2cef4cd773cd50
Egor Naugolnyy <e.naugolnyy@vk.team>
2022-04-05 17:18:42 +0300

    CORE-3612 add exampleTest for GooglePlay methods

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,6 +13,12 @@ use Mycom\Tracker\S2S\Api\CustomEventMethod;
 use Mycom\Tracker\S2S\Api\CustomRevenueBatchMethod;
 use Mycom\Tracker\S2S\Api\CustomRevenueMethod;
 use Mycom\Tracker\S2S\Api\Example;
+use Mycom\Tracker\S2S\Api\GooglePlayProductTransactionBatchMethod;
+use Mycom\Tracker\S2S\Api\GooglePlayProductTransactionMethod;
+use Mycom\Tracker\S2S\Api\GooglePlaySubscriptionTokenBatchMethod;
+use Mycom\Tracker\S2S\Api\GooglePlaySubscriptionTokenMethod;
+use Mycom\Tracker\S2S\Api\GooglePlaySubscriptionTransactionBatchMethod;
+use Mycom\Tracker\S2S\Api\GooglePlaySubscriptionTransactionMethod;
 use Mycom\Tracker\S2S\Api\LoginBatchMethod;
 use Mycom\Tracker\S2S\Api\LoginMethod;
 use Mycom\Tracker\S2S\Api\RegistrationBatchMethod;
@@ -400,5 +406,291 @@ class ExampleTest extends TestCase
             });
 
         Example::sendCustomRevenueBatch(1, 'secret', $client);
+    }
+
+    /**
+     * @covers ::sendGooglePlayProductTransaction
+     * @return void
+     */
+    public function testSendGooglePlayProductTransaction(): void
+    {
+        $client = $this->createMock(ClientInterface::class);
+        $client
+            ->expects(self::at(0))
+            ->method('request')
+            ->willReturnCallback(static function (MethodInterface $method): ResponseInterface {
+                self::assertInstanceOf(GooglePlayProductTransactionMethod::class, $method);
+                self::assertEquals('googlePlayProductTransaction', $method->getUri());
+                self::assertEquals([
+                    RequestOptions::HEADERS => [ClientInterface::AUTH_HEADER_NAME => 'secret'],
+                    RequestOptions::QUERY => ['idApp' => 1],
+                    RequestOptions::JSON => [
+                        'customUserId' => '100500',
+                        'orderId' => '234-1234-1234-12345',
+                        'productId' => '001',
+                        'token' => 'ofjkingojelmkmedpgfkfelj',
+                        'currency' => 'USD',
+                        'revenue' => 10.0,
+                        'eventTimestamp' => time()
+                    ],
+                ], $method->getRequestOptions());
+                return new Response(200, [], 'OK');
+            });
+        $client
+            ->expects(self::at(1))
+            ->method('request')
+            ->willReturnCallback(static function (MethodInterface $method): ResponseInterface {
+                self::assertInstanceOf(GooglePlayProductTransactionMethod::class, $method);
+                self::assertEquals('googlePlayProductTransaction', $method->getUri());
+                self::assertEquals([
+                    RequestOptions::HEADERS => [ClientInterface::AUTH_HEADER_NAME => 'secret'],
+                    RequestOptions::QUERY => ['idApp' => 1],
+                    RequestOptions::JSON => [
+                        'customUserId' => '500100',
+                        'orderId' => '321-4321-4321-54321',
+                        'productId' => '002',
+                        'token' => 'jlefkfgpdemkmlejognikjfo',
+                        'currency' => 'USD',
+                        'revenue' => 20.0,
+                        'eventTimestamp' => time()
+                    ],
+                ], $method->getRequestOptions());
+                return new Response(200, [], 'OK');
+            });
+
+        Example::sendGooglePlayProductTransaction(1, 'secret', $client);
+    }
+
+    /**
+     * @covers ::sendGooglePlayProductTransactionBatch
+     * @return void
+     */
+    public function testSendGooglePlayProductTransactionBatch(): void
+    {
+        $client = $this->createMock(ClientInterface::class);
+        $client
+            ->expects(self::once())
+            ->method('request')
+            ->willReturnCallback(static function (MethodInterface $method): ResponseInterface {
+                self::assertInstanceOf(GooglePlayProductTransactionBatchMethod::class, $method);
+                self::assertEquals('googlePlayProductTransactionBatch', $method->getUri());
+                self::assertEquals([
+                    RequestOptions::HEADERS => [ClientInterface::AUTH_HEADER_NAME => 'secret'],
+                    RequestOptions::QUERY => ['idApp' => 1],
+                    RequestOptions::JSON => [
+                        [
+                            'customUserId' => '100500',
+                            'orderId' => '234-1234-1234-12345',
+                            'productId' => '001',
+                            'token' => 'ofjkingojelmkmedpgfkfelj',
+                            'currency' => 'USD',
+                            'revenue' => 10.0,
+                            'eventTimestamp' => time()
+                        ],
+                        [
+                            'customUserId' => '500100',
+                            'orderId' => '321-4321-4321-54321',
+                            'productId' => '002',
+                            'token' => 'jlefkfgpdemkmlejognikjfo',
+                            'currency' => 'USD',
+                            'revenue' => 20.0,
+                            'eventTimestamp' => time()
+                        ],
+                    ],
+                ], $method->getRequestOptions());
+                return new Response(200, [], 'OK');
+            });
+
+        Example::sendGooglePlayProductTransactionBatch(1, 'secret', $client);
+    }
+
+    /**
+     * @covers ::sendGooglePlaySubscriptionTransaction
+     * @return void
+     */
+    public function testSendGooglePlaySubscriptionTransaction(): void
+    {
+        $client = $this->createMock(ClientInterface::class);
+        $client
+            ->expects(self::at(0))
+            ->method('request')
+            ->willReturnCallback(static function (MethodInterface $method): ResponseInterface {
+                self::assertInstanceOf(GooglePlaySubscriptionTransactionMethod::class, $method);
+                self::assertEquals('googlePlaySubscriptionTransaction', $method->getUri());
+                self::assertEquals([
+                    RequestOptions::HEADERS => [ClientInterface::AUTH_HEADER_NAME => 'secret'],
+                    RequestOptions::QUERY => ['idApp' => 1],
+                    RequestOptions::JSON => [
+                        'customUserId' => '100500',
+                        'orderId' => '234-1234-1234-12345',
+                        'eventTimestamp' => time(),
+                        'priceCurrencyCode' => 'USD',
+                        'priceAmountMicros' => 1990000,
+                        'subscriptionId' => 'monthly001'
+                    ],
+                ], $method->getRequestOptions());
+                return new Response(200, [], 'OK');
+            });
+        $client
+            ->expects(self::at(1))
+            ->method('request')
+            ->willReturnCallback(static function (MethodInterface $method): ResponseInterface {
+                self::assertInstanceOf(GooglePlaySubscriptionTransactionMethod::class, $method);
+                self::assertEquals('googlePlaySubscriptionTransaction', $method->getUri());
+                self::assertEquals([
+                    RequestOptions::HEADERS => [ClientInterface::AUTH_HEADER_NAME => 'secret'],
+                    RequestOptions::QUERY => ['idApp' => 1],
+                    RequestOptions::JSON => [
+                        'customUserId' => '500100',
+                        'orderId' => '234-1234-1234-54321',
+                        'eventTimestamp' => time(),
+                        'priceCurrencyCode' => 'USD',
+                        'priceAmountMicros' => 5990000,
+                        'subscriptionId' => 'monthly002',
+                        'paymentState' => '1',
+                        'isIntroductory' => 0,
+                        'startTimeMillis' => 1693242344000,
+                        'expiryTimeMillis' => 1693242344000,
+                    ],
+                ], $method->getRequestOptions());
+                return new Response(200, [], 'OK');
+            });
+
+        Example::sendGooglePlaySubscriptionTransaction(1, 'secret', $client);
+    }
+
+    /**
+     * @covers ::sendGooglePlaySubscriptionTransactionBatch
+     * @return void
+     */
+    public function testSendGooglePlaySubscriptionTransactionBatch(): void
+    {
+        $client = $this->createMock(ClientInterface::class);
+        $client
+            ->expects(self::once())
+            ->method('request')
+            ->willReturnCallback(static function (MethodInterface $method): ResponseInterface {
+                self::assertInstanceOf(GooglePlaySubscriptionTransactionBatchMethod::class, $method);
+                self::assertEquals('googlePlaySubscriptionTransactionBatch', $method->getUri());
+                self::assertEquals([
+                    RequestOptions::HEADERS => [ClientInterface::AUTH_HEADER_NAME => 'secret'],
+                    RequestOptions::QUERY => ['idApp' => 1],
+                    RequestOptions::JSON => [
+                        [
+                            'customUserId' => '100500',
+                            'orderId' => '234-1234-4321-12345',
+                            'priceCurrencyCode' => 'USD',
+                            'eventTimestamp' => time(),
+                            'priceAmountMicros' => 1990000,
+                            'subscriptionId' => 'monthly001'
+                        ],
+                        [
+                            'customUserId' => '500100',
+                            'orderId' => '2345-1234-1234-12345',
+                            'eventTimestamp' => time(),
+                            'priceCurrencyCode' => 'USD',
+                            'priceAmountMicros' => 5990000,
+                            'subscriptionId' => 'monthly002',
+                            'paymentState' => '1',
+                            'isIntroductory' => 0,
+                            'startTimeMillis' => 1693242344000,
+                            'expiryTimeMillis' => 1693242344000,
+                        ],
+                    ],
+                ], $method->getRequestOptions());
+                return new Response(200, [], 'OK');
+            });
+
+        Example::sendGooglePlaySubscriptionTransactionBatch(1, 'secret', $client);
+    }
+
+    /**
+     * @covers ::sendGooglePlaySubscriptionToken
+     * @return void
+     */
+    public function testSendGooglePlaySubscriptionToken(): void
+    {
+        $client = $this->createMock(ClientInterface::class);
+        $client
+            ->expects(self::at(0))
+            ->method('request')
+            ->willReturnCallback(static function (MethodInterface $method): ResponseInterface {
+                self::assertInstanceOf(GooglePlaySubscriptionTokenMethod::class, $method);
+                self::assertEquals('googlePlaySubscriptionToken', $method->getUri());
+                self::assertEquals([
+                    RequestOptions::HEADERS => [ClientInterface::AUTH_HEADER_NAME => 'secret'],
+                    RequestOptions::QUERY => ['idApp' => 1],
+                    RequestOptions::JSON => [
+                        'customUserId' => '100500',
+                        'orderId' => '234-1234-1234-12345',
+                        'eventTimestamp' => time(),
+                        'token' => 'ofjkingojelmkmedpgfkfelj',
+                        'subscriptionId' => 'monthly001'
+                    ],
+                ], $method->getRequestOptions());
+                return new Response(200, [], 'OK');
+            });
+        $client
+            ->expects(self::at(1))
+            ->method('request')
+            ->willReturnCallback(static function (MethodInterface $method): ResponseInterface {
+                self::assertInstanceOf(GooglePlaySubscriptionTokenMethod::class, $method);
+                self::assertEquals('googlePlaySubscriptionToken', $method->getUri());
+                self::assertEquals([
+                    RequestOptions::HEADERS => [ClientInterface::AUTH_HEADER_NAME => 'secret'],
+                    RequestOptions::QUERY => ['idApp' => 1],
+                    RequestOptions::JSON => [
+                        'customUserId' => '100500',
+                        'orderId' => '234-1234-1234-12345',
+                        'eventTimestamp' => time(),
+                        'token' => 'jlefkfgpdemkmlejognikjfo',
+                        'subscriptionId' => 'monthly001',
+                        'subscriptionPeriod' => 'P1M'
+                    ],
+                ], $method->getRequestOptions());
+                return new Response(200, [], 'OK');
+            });
+
+        Example::sendGooglePlaySubscriptionToken(1, 'secret', $client);
+    }
+
+    /**
+     * @covers ::sendGooglePlaySubscriptionTokenBatch
+     * @return void
+     */
+    public function testSendGooglePlaySubscriptionTokenBatch(): void
+    {
+        $client = $this->createMock(ClientInterface::class);
+        $client
+            ->expects(self::once())
+            ->method('request')
+            ->willReturnCallback(static function (MethodInterface $method): ResponseInterface {
+                self::assertInstanceOf(GooglePlaySubscriptionTokenBatchMethod::class, $method);
+                self::assertEquals('googlePlaySubscriptionTokenBatch', $method->getUri());
+                self::assertEquals([
+                    RequestOptions::HEADERS => [ClientInterface::AUTH_HEADER_NAME => 'secret'],
+                    RequestOptions::QUERY => ['idApp' => 1],
+                    RequestOptions::JSON => [
+                        [
+                            'customUserId' => '100500',
+                            'orderId' => '234-1234-1234-12345',
+                            'eventTimestamp' => time(),
+                            'subscriptionId' => 'monthly001',
+                            'token' => 'ofjkingojelmkmedpgfkfelj'
+                        ],
+                        [
+                            'customUserId' => '100500',
+                            'orderId' => '234-1234-1234-12345',
+                            'eventTimestamp' => time(),
+                            'subscriptionId' => 'monthly001',
+                            'token' => 'jlefkfgpdemkmlejognikjfo',
+                            'subscriptionPeriod' => 'P1M',
+                        ],
+                    ],
+                ], $method->getRequestOptions());
+                return new Response(200, [], 'OK');
+            });
+
+        Example::sendGooglePlaySubscriptionTokenBatch(1, 'secret', $client);
     }
 }
